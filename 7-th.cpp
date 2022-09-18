9a6d9c7b783d413e0a4d139f09445d80fbf8c029
KATAROTA <112807542+KATAROTA@users.noreply.github.com>
2022-09-18 06:02:42 -0600

    Add files via upload

---
+++ 7-th.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void calculator (int n1, int n2);
+
+int main() {
+		int numbers[5] = { 1,2,3,4,5 };
+		cout << numbers[0] <<endl;
+		cout << numbers[1] <<endl;
+		cout << numbers[4] <<endl;
+		numbers[2] = 100;
+		cout << numbers[2] << endl;
+		
+		return 0;
+		
+		
+		
+		
+}
